fe8d5a38a7cb40010c14637449eb1425d70442d5
xyf <2316639449@qq.com>
2024-03-27 10:27:24 +0000

    add ctrl+z for undo and ctrl + q for quit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,12 @@
 vim.api.nvim_set_keymap("n", "<A-Right>", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-Left>", ":bprevious<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-q>",
+  ":if tabpagenr('$') > 1 | tabclose | else | quit | endif<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "so", ":only<CR>", { noremap = true, silent = true })
