145450e8941c59f05eeac978227300b2a7be34a8
xyf <2316639449@qq.com>
2024-06-20 21:37:35 +0100

    update some keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,28 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "so", ":only<CR>", { noremap = true, silent = true })
+
+-- Bind Alt+; to jump to the beginning of the line
+vim.api.nvim_set_keymap("n", "<M-;>", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-;>", "<Esc>^i", { noremap = true, silent = true })
+
+-- Bind Alt+' to jump to the end of the line
+vim.api.nvim_set_keymap("n", "<M-'>", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-'>", "<Esc>A", { noremap = true, silent = true })
+
+-- Move cursor to the beginning of the current or previous word and enter insert mode with Alt+.
+vim.api.nvim_set_keymap("n", "<M-.>", "bi", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-.>", "<Esc>bi", { noremap = true, silent = true })
+
+-- Move cursor to the end of the current word and enter insert mode with Alt+/
+vim.api.nvim_set_keymap("n", "<M-/>", "ea", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-/>", "<Esc>ea", { noremap = true, silent = true })
+
+-- Set timeoutlen to 200 milliseconds
+vim.o.timeoutlen = 200
+
+-- Map Alt+r to save the current file in normal mode
+vim.api.nvim_set_keymap("n", "<M-r>", ":w<CR>", { noremap = true, silent = true })
+
+-- Map Alt+r to save the current file in insert mode
+vim.api.nvim_set_keymap("i", "<M-r>", "<Esc>:w<CR>a", { noremap = true, silent = true })
